91f3a9840d90faf7906569dd7a84b9fa3e4fe112
Alexander Bukin <lxndrbukin@proton.me>
2025-11-04 19:39:21 +0000

    Major OOP Refactor done for the Task Manager CLI. Converted into TaskManager class with all the functions refactored into methods.

--- main.py
+++ main.py
@@ -2,68 +2,79 @@ import json
 import os
 from tabulate import tabulate
 
-def todo_list_manager():
-    file_path = os.path.join(os.path.dirname(__file__), "tasks.json")
-    if not os.path.exists(file_path):
-        with open(file_path, "w") as file:
-            json.dump([], file)
+class TaskManager:
+    def __init__(self, file_path=os.path.join(os.path.dirname(__file__), "tasks.json")):
+        self._file_path = file_path
+    
+    @property
+    def file_path(self):
+        return self._file_path
 
-    def create_task(task):
-        with open(file_path, "r") as file:
+    @file_path.setter
+    def file_path(self, path):
+        self._file_path = path
+
+    def fetch_data(self):
+        with open(self.file_path, "r") as file:
             data = json.load(file)
+        return data
+
+    def save_data(self, data):
+        with open(self.file_path, "w") as file:
+            json.dump(data, file)
+
+    def create_entry(self, task):
+        data = self.fetch_data()
         if not data:
             data = [{"id": 1, "task": task}]
         else:
             max_id = max(data, key=lambda x: x.get("id", float("-inf")))["id"] + 1
             data.append({'id': max_id, 'task': task})
-        with open(file_path, "w") as file:
-            json.dump(data, file)
-
-    def get_tasks():
-        with open(file_path, "r") as file:
-            data = json.load(file)
-        return data
-
-    def delete_task(task_id):
-        with open(file_path, "r") as file:
-            data = json.load(file)
+        self.save_data(data)
+    
+    def remove_entry(self, task_id):
+        data = self.fetch_data()
         for i, task in enumerate(data):
             if task.get("id") == task_id:
                 del data[i]
-                with open(file_path, "w") as file:
-                    json.dump(data, file)
+                self.save_data(data)
                 return
-        print_message("Task not found. Please enter a valid task ID.")
-
-    def format_task_list(tasks):
-        print_message("Full list of tasks is shown below:", 34)
-        table_data = [[item['id'], item['task']] for item in tasks]
-        print(tabulate(table_data, headers=["ID", "Task"], tablefmt="grid") + "\n")
-
+    
+    @staticmethod
     def print_message(message, color=31):
         print(f"\033[{color};1m\n{message}\033[0m\n")
 
-    while True:
-        try:
-            option = int(input(
-                f"Please select an option:\n1. Add Task.\n2. View Tasks.\n3. Delete Task.\nEnter option number: "))
-            if option == 1:
-                input_text = str(input(f"Enter and submit task: "))
-                create_task(input_text)
-                print_message("Task created successfully.", 32)
-            if option == 3:
-                format_task_list(get_tasks())
-                task_id = int(input(f"Please enter the task ID to delete: "))
-                delete_task(task_id)
-                print_message(f"Task under ID {task_id} has been successfully deleted.", 32)
-            elif option > 3 or option < 1:
-                print_message("Please select from options 1-3")
-            format_task_list(get_tasks())
-        except ValueError:
-            print_message("Please enter a number")
-        except KeyboardInterrupt:
-            print_message("Program stopped by user")
-            break
+    def format_task_list(self, tasks):
+        self.print_message("Full list of tasks is shown below:", 34)
+        table_data = [[item['id'], item['task']] for item in tasks]
+        print(tabulate(table_data, headers=["ID", "Task"], tablefmt="grid") + "\n")
+
+    def run_cli(self):
+        while True:
+            try:
+                options = ["Add Task", "View Tasks", "Delete Task"]
+                print("Please select an option:")
+                for i, option in enumerate(options, 1):
+                    print(f"{i}. {option}")
+                selected = int(input(
+                    f"Enter option number: "))
+                if selected == 1:
+                    input_text = str(input(f"Enter and submit task: "))
+                    self.create_entry(input_text)
+                    self.print_message("Task created successfully.", 32)
+                if selected == 3:
+                    self.format_task_list(self.fetch_data())
+                    task_id = int(input(f"Please enter the task ID to delete: "))
+                    self.remove_entry(task_id)
+                    self.print_message(f"Task under ID {task_id} has been successfully deleted.", 32)
+                elif selected > 3 or selected < 1:
+                    self.print_message("Please select from options 1-3")
+                self.format_task_list(self.fetch_data())
+            except ValueError:
+                self.print_message("Please enter a number")
+            except KeyboardInterrupt:
+                self.print_message("Program stopped by user")
+                break
 
 if __name__ == "__main__":
-    todo_list_manager()
+    TaskManager().run_cli()
